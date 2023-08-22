6f399723a9c829278964bb121331e8c946d1c680
Diogo Santos <diiogosantos@live.com>
2023-08-22 13:25:29 -0300

    bbbb

--- teste.sql
+++ teste.sql
@@ -13,16 +13,26 @@ CREATE TABLE customers2 (
 );
 
 
---selecionando itens na tabela 
+--selecionando itens na tabela -- 
 
-SELECT * FROM customers2;
+SELECT * FROM customers;
 
 SELECT
     name,
     email
 FROM customers ;
 
---visualizando estrutura de uma tabela
+--visualizando estrutura de uma tabela --
 
 PRAGMA table_info('customers');
+
+-- sempre respeitando a ordem das propriedades --
+INSERT INTO customers (id, name, email, age)
+VALUES ('C002', 'Coliro', 'coliro@email.com', 40);
+
+-- editar tabela / where é o marcador pra determninar onde se deve atualizar --
+UPDATE customers
+    SET age = 41
+WHERE id = 'C002';
+
 
